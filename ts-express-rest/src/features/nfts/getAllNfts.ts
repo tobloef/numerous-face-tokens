605c365744967205943fb66e24e9c8ce4da634e9
Tobias Løfgren <tobloef@gmail.com>
2021-10-29 11:31:14 +0200

    getAllNfts

---
+++ ts-express-rest/src/features/nfts/getAllNfts.ts
@@ -0,0 +1,28 @@
+import { Nft, UserWithPassword } from ".prisma/client";
+import express from "express";
+import { ok } from "neverthrow";
+import Feature from "../../types/feature";
+import SetupRequest from "../../types/SetupRequest";
+import User from "../../types/User";
+import deleteProp from "../../utils/deleteProp";
+ 
+type GetAllNftsRequest = {
+    // TODO: Filtering
+};
+
+type GetAllNftsResponse = Nft[];
+
+export const getAllNfts: Feature<GetAllNftsRequest, GetAllNftsResponse> = async (
+    request,
+    ctx,
+) => {
+    const nfts = await ctx.prisma.nft.findMany();
+
+    return ok(nfts);
+};
+
+export const setupGetAllNftsRequest: SetupRequest<GetAllNftsRequest> = (
+    req: express.Request,
+) => {
+    return ok({});
+}
