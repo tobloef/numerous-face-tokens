1e19fb253bdf1ba8f49cb71df0dc26eddfc7c568
Tobias Løfgren <tobloef@gmail.com>
2021-10-30 14:17:49 +0200

    acceptTrade

--- ts-express-rest/src/features/trades/acceptTrade.ts
+++ ts-express-rest/src/features/trades/acceptTrade.ts
@@ -2,15 +2,15 @@ import express, { request } from "express";
 import { is } from "typescript-is";
 import { err, ok } from "neverthrow";
 import ApiError from "../../ApiError";
-import Feature from "../../types/feature";
+import { PrivateFeature } from "../../types/feature";
 import SetupRequest from "../../types/SetupRequest";
 import { Nft, Trade } from "@prisma/client";
 import User from "../../types/User";
 import deleteProp from "../../utils/deleteProp";
+import assert from "assert";
 
 type AcceptTradeRequest = {
-    tradeId: number,
-    accepterUsername: string,
+    tradeId: string,
 };
 
 type AcceptTradeResponse = 
@@ -21,7 +21,7 @@ type AcceptTradeResponse =
         buyer: User;
     };
 
-export const acceptTrade: Feature<AcceptTradeRequest, AcceptTradeResponse> = async (
+export const acceptTrade: PrivateFeature<AcceptTradeRequest, AcceptTradeResponse> = async (
     request,
     ctx,
 ) => {
@@ -29,21 +29,85 @@ export const acceptTrade: Feature<AcceptTradeRequest, AcceptTradeResponse> = asy
         where: {
             id: request.tradeId,
         },
+        include: {
+            buyer: true,
+            seller: true,
+        }
     });
 
     if (trade == null) {
         return err(new ApiError("Trade not found", 404));
     }
-    
-    if (trade.sellerId === request.accepterUsername) {
 
-        return ok();
-    } else if (trade.buyerId === request.accepterUsername) {
+    assert(trade.buyer !== null);
+    assert(trade.buyerId !== null);
+    assert(trade.seller !== null);
+    assert(trade.sellerId !== null);
 
-        return ok();
-    } else {
-        return err(new ApiError("Cannot accept trade you are not participating in", 400));
+    if (
+        trade.sellerId !== ctx.user.id &&
+        trade.buyerId !== ctx.user.id
+    ) {
+        return err(new ApiError("Cannot accept trade you are not participating in", 403));
     }
+
+    if (
+        (trade.sellerId === ctx.user.id && trade.sellerAccepted) ||
+        (trade.buyerId === ctx.user.id && trade.buyerAccepted)
+    ) {
+        return err(new ApiError("Already accepted", 400));
+    }
+
+    if (trade.buyer.balance >= trade.price) {
+        return err(new ApiError("Insufficient funds", 400));
+    }
+
+    const updatedTradeWithPasswords = await ctx.prisma.trade.update({
+        where: {
+            id: request.tradeId,
+        },
+        include: {
+            nft: true,
+            buyer: true,
+            seller: true,
+        },
+        data: {
+            ...(trade.buyerId === ctx.user.id ? {
+                buyerAccepted: true,
+                buyerAcceptedAt: new Date(),
+            } : {}),
+            ...(trade.sellerId === ctx.user.id ? {
+                sellerAccepted: true,
+                sellerAcceptedAt: new Date(),
+            } : {}),
+            buyer: {
+                update: {
+                    balance: trade.seller.balance - trade.price,
+                }
+            },
+            seller: {
+                update: {
+                    balance: trade.seller.balance + trade.price,
+                }
+            },
+            nft: {
+                update: {
+                    ownerId: trade.buyerId
+                }
+            }
+        }
+    });
+
+    assert(updatedTradeWithPasswords.buyer !== null);
+    assert(updatedTradeWithPasswords.seller !== null);
+
+    const updatedTrade = {
+        ...updatedTradeWithPasswords,
+        seller: deleteProp(updatedTradeWithPasswords.seller, "passwordHash"),
+        buyer: deleteProp(updatedTradeWithPasswords.buyer, "passwordHash"),
+    };
+
+    return ok(updatedTrade);
 };
 
 export const setupAcceptTradeRequest: SetupRequest<AcceptTradeRequest> = (
@@ -51,10 +115,6 @@ export const setupAcceptTradeRequest: SetupRequest<AcceptTradeRequest> = (
 ) => {
     if (!is<AcceptTradeRequest>(req.body)) {
         return err(new ApiError("Invalid trade acceptation", 400));
-    }
-
-    if (req.body.accepterUsername !== req.user.username) {
-        return err(new ApiError("Cannot accept trade for someone else", 400));
     }
     
     return ok(req.body);
