7b4dcf7b7b72b273171e874367945f1ef5e2564d
Tobias Løfgren <tobloef@gmail.com>
2021-11-14 22:33:41 +0100

    Center main content

--- react-vite/src/shared/MainRoutes.tsx
+++ react-vite/src/shared/MainRoutes.tsx
@@ -11,29 +11,32 @@ import TradesOverview from "../trades/TradesOverview";
 import TradeDetails from "../trades/TradeDetails";
 import NftDetails from "../nfts/NftDetails";
 import Login from "../auth/Login";
+import classes from "./MainRoutes.module.css";
 
-const MainRoutes = ({}) => {
-    return <div>
-        <Routes>
-            <Route path="/">
-                <Route index element={<Dashboard />} />
-                <Route path="users">
-                    <Route index element={<UsersOverview />} />
-                    <Route path=":username" element={<UserDetails />} />
-                </Route>
-                <Route path="nfts">
-                    <Route index element={<NftsOverview />} />
-                    <Route path=":seed" element={<NftDetails />} />
-                </Route>
-                <Route path="trades">
-                    <Route index element={<TradesOverview />} />
-                    <Route path=":tradeId" element={<TradeDetails />} />
-                </Route>
-                <Route path="register" element={<Register />} />
-                <Route path="login" element={<Login />} />
-            </Route>
-        </Routes>
-    </div>
-};
+const MainRoutes = ({}) => (
+  <div className={classes.mainWrapper}>
+      <div className={classes.mainContent}>
+          <Routes>
+              <Route path="/">
+                  <Route index element={<Dashboard/>}/>
+                  <Route path="users">
+                      <Route index element={<UsersOverview/>}/>
+                      <Route path=":username" element={<UserDetails/>}/>
+                  </Route>
+                  <Route path="nfts">
+                      <Route index element={<NftsOverview/>}/>
+                      <Route path=":seed" element={<NftDetails/>}/>
+                  </Route>
+                  <Route path="trades">
+                      <Route index element={<TradesOverview/>}/>
+                      <Route path=":tradeId" element={<TradeDetails/>}/>
+                  </Route>
+                  <Route path="register" element={<Register/>}/>
+                  <Route path="login" element={<Login/>}/>
+              </Route>
+          </Routes>
+      </div>
+  </div>
+);
 
 export default MainRoutes;
