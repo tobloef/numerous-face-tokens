0659990ad6da69e654a4b33f794e2f4b92160555
Tobias Løfgren <tobloef@gmail.com>
2021-11-24 17:18:22 +0100

    WIP dashboard

--- react-vite/src/dashboard/Dashboard.tsx
+++ react-vite/src/dashboard/Dashboard.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import styles from "./Dashboard.module.css";
+import { CURRENCY_SYMBOL } from "../../../express-rest/src/utils/constants";
 
 const Dashboard: React.FC<{
 
 }> = (props) => {
-  // TODO
-  return null;
+  return (
+    <div className={styles.dashboard}>
+      <div className={styles.infoWrapper}>
+        <p className={styles.header}>
+          Welcome Numerous Face Tokens
+        </p>
+        <p className={styles.explainer}>
+          This site is a mock trading platform for Numerous Face Tokens (NFTs).
+          Each NFT is randomly generated from a specified "seed" that you enter when you create a new NFT.
+          When signing up, you automatically gain {CURRENCY_SYMBOL}1000, the currency of this site,
+          which you can use to buy and sell NFTs.
+          <br />
+          <br />
+          Numerous Face Tokens was created as an attempt to compare multiple web technology stacks
+          and you can find the source code <a href={"https://github.com/tobloef/numerous-face-tokens"}>on GitHub</a>.
+          The version of the site you are currently viewing, was made with TypeScript, React.js, Express.js and Prisma.
+        </p>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
 };
 
 export default Dashboard;
